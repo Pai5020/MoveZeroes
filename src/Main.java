9604205f28d2a47dd64ef1dfc3cff36219e74004
PARVATHY SUGUNAN <parvathysugunan5020@gmail.com>
2024-05-02 12:42:45 +0530

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,37 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int i,j ;
+        int i, j;
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[6];
         System.out.println("Enter the array elements");
-        for(i=0;i<arr.length;i++){
-            arr[i]=sc.nextInt();
+        for (i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
-        int count= 0;
-        for(i=0;i< arr.length;i++){
-            if(arr[i]!=0){
-                arr[count++]=arr[i];
+        System.out.println("The array after moving the zeroes to one side : ");
+
+        for (i = 0,j= arr.length-1; i <= j; ) {
+            if (arr[i] == 0) {
+                if (arr[j] != 0) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
+                j--;
+
             }
-        for(;count< arr.length;count++){
-            arr[count]=0;
+            else if(arr[i]==0 && arr[j] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+            }
+
+            else {
+                i++;
+            }
         }
-        for(i=0;i<arr.length;i++){
+        for(i=0;i< arr.length;i++)
             System.out.println(arr[i]);
-        }
-        }
+    }
 }
